3cd5c8e212fb6c58ce712d87aacfa0f448eacaab
ninfant <noilsa@icloud.com>
2025-02-02 16:13:23 +0200

    Daily Challenge

---
+++ Week1/Day1/Daily/daily_ch_exercises.py
@@ -0,0 +1,32 @@
+
+import random
+##ex1
+# user_string =(input('Please enter a string: '))
+# len_variable= int(len(user_string))
+# if (len_variable == 10):
+#     print('Perfect string')
+# elif(len_variable < 10):
+#     print('String not long enough')
+
+# else:
+#     print('String too long')
+
+
+# #ex2
+# userstring='HelloWorld'
+# print(userstring[0])
+# print(userstring[9])
+
+# #ex3
+# user_string = "HelloWorld"
+# output = "" 
+# for i in user_string:
+#     output += i 
+#     print(output)  
+
+# #ex4
+# userstring='HelloWorld'
+# converted_list=list(userstring)
+# random.shuffle(converted_list)
+# shuffled_text="".join(converted_list)
+# print(shuffled_text)
