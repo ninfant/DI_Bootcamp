142e08c6392cf5a696eb05bc434b4d8fa1dc7106
ninfant <noilsa@icloud.com>
2025-02-04 00:58:53 +0200

    Daily Challenge

---
+++ Week1/Day2/Daily_Challenge/daily_ch.py
@@ -0,0 +1,21 @@
+#ex1
+# num = int(input("Enter a number: ")) 
+# length = int(input("Enter a length: "))
+# multiples_list = []
+# for i in range(1, length + 1): 
+#     multiple = num * i 
+#     multiples_list.append(multiple) 
+
+# print("Multiples List:", multiples_list)
+
+
+
+
+#ex2
+# user_input = input("Enter a word: ")
+# new_string = ""
+# for i in range(len(user_input)):
+#     if i == 0 or user_input[i] != user_input[i - 1]:  #Keep only non-consecutive duplicates
+#         new_string += user_input[i]
+
+# print("New word:", new_string)
