c8042e26b1d7bd7f16d018c0392c4367593f036f
ninfant <noilsa@icloud.com>
2025-02-06 01:03:29 +0200

    Daily_Challenge

---
+++ Week1/Day4/Daily_Challange/daily_challange.py
@@ -0,0 +1,24 @@
+#ex1
+# matrix = [
+#     ['7', 'i', 'i'],
+#     ['T', 's', 'x'],
+#     ['h', '%', '?'],
+#     ['i', ' ', '#'],
+#     ['s', 'M', ' '],
+#     ['$', 'a', ' '],
+#     ['#', 't', '%'],
+#     ['^', 'r', '!']
+# ]
+
+# def decode_matrix(matrix):
+#     rows = len(matrix)
+#     cols = len(matrix[0])
+    
+#     decrypted_text = []
+    
+#     for col in range(cols):
+#         for row in range(rows):
+#             decrypted_text.append(matrix[row][col])
+
+#TO THE CHECKER
+#I started with the exercise but i didnt understand it as i should, so prefer to check it with your help
