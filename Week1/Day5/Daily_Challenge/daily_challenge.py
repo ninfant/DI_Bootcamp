6a1fb1ab0dee2756323334a48b356e328bb40e3a
ninfant <noilsa@icloud.com>
2025-02-09 02:06:26 +0200

    Daily_Challenge

---
+++ Week1/Day5/Daily_Challenge/daily_challenge.py
@@ -0,0 +1,23 @@
+#ex1
+
+# user_sequence = input('Enter the comma separated sequence of words: ')
+# words_list = [word.strip() for word in user_sequence.split(",")]
+# sorted_user_sequence = sorted(words_list)
+# print(sorted_user_sequence)
+# print(f"Palabras ordenadas: {",".join(sorted_user_sequence)}")
+
+
+#ex2
+# def longest_word(sentence):
+#     max_length = 0
+#     longest = ''
+#     user_sentence = sentence.split()
+#     for word in user_sentence:
+#         if len(word) > max_length:
+#             max_length = len(word)
+#             longest = word
+            
+#     return longest     
+
+
+# print(longest_word('Forgetfulness is by all means powerless!'))
