35c08d64ba69f3de6d1ba9b7f44e7dddf411f00b
ninfant <noilsa@icloud.com>
2025-02-16 16:02:17 +0200

    XP GOLD

---
+++ Week3/Day1/Exrecises XP Gold/xp_ gold_exrecises.sql
@@ -0,0 +1,18 @@
+-- Fetch the first four students. You have to order the four students alphabetically by last_name.
+-- SELECT first_name, last_name, birth_date
+-- FROM students
+-- ORDER BY last_name ASC
+-- LIMIT 4;
+
+-- SELECT * FROM students
+
+-- Fetch the details of the youngest student.
+-- SELECT first_name, last_name, birth_date 
+-- FROM students 
+-- ORDER BY birth_date DESC 
+-- LIMIT 1;
+
+-- Fetch three students skipping the first two students.
+-- SELECT first_name, last_name, birth_date 
+-- FROM students 
+-- LIMIT 3 OFFSET 2;
