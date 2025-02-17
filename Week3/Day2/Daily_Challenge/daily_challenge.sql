e943e473e8ecba92a7a5c03698a7dca2566b1fcd
ninfant <noilsa@icloud.com>
2025-02-17 23:50:00 +0200

    Daily_challenge

---
+++ Week3/Day2/Daily_Challenge/daily_challenge.sql
@@ -0,0 +1,33 @@
+-- CREATE TABLE FirstTab (
+--      id integer, 
+--      name VARCHAR(10)
+-- )
+
+-- INSERT INTO FirstTab 
+-- VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar')
+
+-- SELECT * FROM FirstTab
+
+-- CREATE TABLE SecondTab (
+--     id integer 
+-- )
+
+-- INSERT INTO SecondTab VALUES
+-- (5),
+-- (NULL)
+
+-- Q1
+-- The output is 0
+
+-- Q2
+-- The output is 2
+
+-- Q3
+-- The output is 0
+
+-- Q4
+-- The output is 2
