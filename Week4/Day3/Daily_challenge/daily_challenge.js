0ccab8d53f8dad084ac0fb6bf763cb29a154169f
ninfant <noilsa@icloud.com>
2025-02-26 09:38:25 +0200

    Daily Challengue

---
+++ Week4/Day3/Daily_challenge/daily_challenge.js
@@ -0,0 +1,4 @@
+// To the Checher
+//As was the Hackathon in the afternoon I really didnt have time to do all during the nigth
+// so i could not do the daily challenge, 
+// i dint want to used chatGPT help just to have the obligation to just send it on time. 
