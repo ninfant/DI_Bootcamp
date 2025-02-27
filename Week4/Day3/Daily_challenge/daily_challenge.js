5692b6d88db5f757057d206de7b398c856038786
ninfant <noilsa@icloud.com>
2025-02-28 01:12:37 +0200

    Daily Challengue

--- Week4/Day3/Daily_challenge/daily_challenge.js
+++ Week4/Day3/Daily_challenge/daily_challenge.js
@@ -1,4 +1,46 @@
-// To the Checher
-//As was the Hackathon in the afternoon I really didnt have time to do all during the nigth
-// so i could not do the daily challenge, 
-// i dint want to used chatGPT help just to have the obligation to just send it on time. 
+// Array of planets with their moons
+const solarSystem = [
+  { name: "Mercury", color: "gray", moons: [] },
+  { name: "Venus", color: "yellow", moons: [] },
+  { name: "Earth", color: "blue", moons: ["Moon"] },
+  { name: "Mars", color: "red", moons: ["Phobos", "Deimos"] },
+  {
+    name: "Jupiter",
+    color: "orange",
+    moons: ["Io", "Europa", "Ganymede", "Callisto"],
+  },
+  {
+    name: "Saturn",
+    color: "goldenrod",
+    moons: ["Titan", "Enceladus", "Mimas"],
+  },
+  {
+    name: "Uranus",
+    color: "lightblue",
+    moons: ["Titania", "Oberon", "Umbriel", "Miranda", "Ariel"],
+  },
+  { name: "Neptune", color: "darkblue", moons: ["Triton", "Proteus"] },
+];
+
+// Select the section where planets will be added
+const section = document.querySelector(".listPlanets");
+
+// Loop through the planets array and create divs
+solarSystem.forEach((planet) => {
+  const planetDiv = document.createElement("div");
+  planetDiv.classList.add("planet");
+  planetDiv.style.backgroundColor = planet.color;
+  planetDiv.textContent = planet.name;
+
+  // Add moons to the planet
+  planet.moons.forEach((moon, index) => {
+    const moonDiv = document.createElement("div");
+    moonDiv.classList.add("moon");
+    moonDiv.style.transform = `rotate(${
+      index * (360 / planet.moons.length)
+    }deg) translate(70px)`; // Position the moons around the planet
+    planetDiv.appendChild(moonDiv);
+  });
+
+  section.appendChild(planetDiv);
+});
