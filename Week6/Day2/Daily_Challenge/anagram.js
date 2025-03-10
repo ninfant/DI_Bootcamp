ab8fa3e5f5f24d0c35eb5a3c7aafc001b7fe156a
ninfant <noilsa@icloud.com>
2025-03-10 23:24:27 +0200

    Daily Challenge Anagram JavaScript

---
+++ Week6/Day2/Daily_Challenge/anagram.js
@@ -0,0 +1,12 @@
+// Create a function that:
+// takes in two strings as two parameters
+// and returns a boolean that indicates whether or not the first string is an anagram of the second string.
+
+// function Anagram(word1, word2){
+//     const firstString = word1.toLowerCase().replace(/[^a-z0-9]/g, '').split('').sort().join('')
+//     const secondString = word2.toLowerCase().replace(/[^a-z0-9]/g, '').split('').sort().join('')
+    
+//     return firstString == secondString
+// }
+
+//Anagram('Astronomer', "Moon starer")
